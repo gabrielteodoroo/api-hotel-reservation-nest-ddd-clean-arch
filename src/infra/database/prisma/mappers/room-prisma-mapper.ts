cbb0501f48cadcca223023d4652624c548502c0f
Teodoro <biel.teodorob@gmail.com>
2024-08-16 09:34:04 -0300

    chore: add room prisma mapper

---
+++ src/infra/database/prisma/mappers/room-prisma-mapper.ts
@@ -0,0 +1,36 @@
+import Identity from '@/core/entities/identity'
+import Room from '@/domain/employee/entities/room'
+import Money from '@/domain/shared/money'
+import { Room as RoomDatabase } from '@prisma/client'
+
+export class RoomPrismaMapper {
+	static toDomain(entity: RoomDatabase): Room {
+		return Room.create(
+			{
+				image: entity.image,
+				name: entity.name,
+				price: Money.create(entity.price),
+				hasAir: entity.hasAir,
+				hasKitchen: entity.hasKitchen,
+				hasWifi: entity.hasWifi,
+				isAvailable: entity.isAvailable,
+				isPetFriendly: entity.isPetFriendly
+			},
+			new Identity(entity.id)
+		)
+	}
+
+	static toPersistence(entity: Room): RoomDatabase {
+		return {
+			id: entity.id.toString(),
+			image: entity.image,
+			name: entity.name,
+			price: entity.price.value,
+			hasAir: entity.hasAir,
+			hasKitchen: entity.hasKitchen,
+			hasWifi: entity.hasWifi,
+			isAvailable: entity.isAvailable,
+			isPetFriendly: entity.isPetFriendly
+		}
+	}
+}
