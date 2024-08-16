3b5c919d92221171d4e8f8c46f9ad448311ddba2
Teodoro <biel.teodorob@gmail.com>
2024-08-16 15:05:48 -0300

    test: implements create booking controller

---
+++ src/infra/http/booking/controllers/cancel-booking.controller.e2e-spec.ts
@@ -0,0 +1,94 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { randomUUID } from 'crypto'
+import { Booking, Employee, Room } from '@prisma/client'
+import { JwtService } from '@nestjs/jwt'
+import { hash } from 'bcrypt'
+
+describe('CancelBookingsController', () => {
+	let app: INestApplication
+	let prisma: PrismaService
+	let jwt: JwtService
+
+	let room: Room
+	let booking: Booking
+	let employee: Employee
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule, DatabaseModule]
+		}).compile()
+
+		app = moduleFixture.createNestApplication()
+		app.useGlobalPipes(new ValidationPipe())
+		jwt = moduleFixture.get(JwtService)
+		prisma = moduleFixture.get(PrismaService)
+
+		await app.init()
+
+		employee = {
+			id: randomUUID(),
+			name: 'estefane',
+			email: 'estefane@email.com',
+			password: await hash('estefane123', 10)
+		}
+
+		room = {
+			id: randomUUID(),
+			name: 'Suíte Presidencial',
+			image: 'quarto.jpg',
+			price: 120000,
+			hasWifi: true,
+			hasAir: true,
+			hasKitchen: true,
+			isPetFriendly: true,
+			isAvailable: true
+		}
+
+		booking = {
+			id: randomUUID(),
+			customer: 'Ana Clara',
+			days: 2,
+			email: 'ana@email.com',
+			isActive: true,
+			roomId: room.id
+		}
+
+		await prisma.employee.create({
+			data: employee
+		})
+
+		await prisma.room.create({
+			data: room
+		})
+
+		await prisma.booking.create({
+			data: booking
+		})
+	})
+
+	test('/bookings/:id (PATCH)', async () => {
+		const token = jwt.sign(employee)
+
+		expect(booking).toHaveProperty('isActive', true)
+
+		const response = await request(app.getHttpServer())
+			.patch(`/bookings/${booking.id}`)
+			.set('Authorization', `Bearer ${token}`)
+			.send()
+
+		expect(response.statusCode).toBe(204)
+
+		const canceledBooking = await prisma.booking.findFirst({
+			where: {
+				id: booking.id
+			}
+		})
+
+		expect(canceledBooking).toHaveProperty('isActive', false)
+	})
+})
