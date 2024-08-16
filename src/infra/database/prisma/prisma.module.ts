4a2572f2ead6d17759271aa58f738ad5ffab14df
Teodoro <biel.teodorob@gmail.com>
2024-08-16 09:26:32 -0300

    chore: prisma module

--- src/infra/database/prisma/prisma.module.ts
+++ src/infra/database/prisma/prisma.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma.service'
+import { EmployeeRepository } from '@/domain/employee/repositories/employee-repository'
+import { EmployeePrismaRepository } from './repositories/employee-prisma-repository'
+import { BookingRepository } from '@/domain/booking/repositories/booking-repository'
+import { BookingPrismaRepository } from './repositories/booking-prisma-repository'
+import { RoomRepository } from '@/domain/employee/repositories/room-repository'
+import { RoomPrismaRepository } from './repositories/room-prisma-repository'
 
 @Module({
-	providers: [PrismaService],
-	exports: [PrismaService]
+	providers: [
+		PrismaService,
+		{ provide: EmployeeRepository, useClass: EmployeePrismaRepository },
+		{ provide: BookingRepository, useClass: BookingPrismaRepository },
+		{ provide: RoomRepository, useClass: RoomPrismaRepository }
+	],
+	exports: [
+		PrismaService,
+		EmployeeRepository,
+		BookingRepository,
+		RoomRepository
+	]
 })
 export class PrismaModule {}
