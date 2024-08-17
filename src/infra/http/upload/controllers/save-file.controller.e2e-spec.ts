cac97b990d738def6d71a48c5b59e068fefb1052
Teodoro <biel.teodorob@gmail.com>
2024-08-17 13:51:31 -0300

    feat: SaveFile controller

---
+++ src/infra/http/upload/controllers/save-file.controller.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from '@/app.module'
+import { hash } from 'bcrypt'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { JwtService } from '@nestjs/jwt'
+import { randomUUID } from 'crypto'
+
+describe('SavefileController', () => {
+	let app: INestApplication
+	let prisma: PrismaService
+	let jwt: JwtService
+
+	let employee: any
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule, DatabaseModule]
+		}).compile()
+
+		app = moduleFixture.createNestApplication()
+		app.useGlobalPipes(new ValidationPipe())
+		prisma = moduleFixture.get(PrismaService)
+		jwt = moduleFixture.get(JwtService)
+
+		await app.init()
+
+		employee = {
+			id: randomUUID(),
+			name: 'estefane',
+			email: 'estefane@email.com',
+			password: await hash('estefane123', 10)
+		}
+
+		await prisma.employee.create({
+			data: employee
+		})
+	})
+
+	test('/upload (POST)', async () => {
+		const token = jwt.sign(employee)
+		const response = await request(app.getHttpServer())
+			.post('/upload')
+			.set('Authorization', `Bearer ${token}`)
+			.attach('file', __dirname + '/fotoquarto.jpg')
+
+		expect(response.statusCode).toBe(200)
+		expect(response.body).toHaveProperty('path')
+	})
+})
