470076f7811b424351275b123414b33b4e23558b
Teodoro <biel.teodorob@gmail.com>
2024-08-17 13:51:26 -0300

    feat: SaveFile controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { EmployeeModule } from './employee/employee.module'
 import { RoomModule } from './room/room.module'
 import { BookingModule } from './booking/booking.module'
+import { UploadFileModule } from './upload/upload-file.module'
 
 @Module({
-	imports: [EmployeeModule, RoomModule, BookingModule],
-	exports: [EmployeeModule, RoomModule, BookingModule]
+	imports: [EmployeeModule, RoomModule, BookingModule, UploadFileModule],
+	exports: [EmployeeModule, RoomModule, BookingModule, UploadFileModule]
 })
 export class HttpModule {}
