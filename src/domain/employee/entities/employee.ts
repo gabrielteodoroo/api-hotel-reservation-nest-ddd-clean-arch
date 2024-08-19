9d0ee7cd33d985ef494e6d1bc730b4209705d2ae
Teodoro <biel.teodorob@gmail.com>
2024-08-18 21:02:48 -0300

    feat: add createdAt and updatedAt properties to Employee entity

--- src/domain/employee/entities/employee.ts
+++ src/domain/employee/entities/employee.ts
@@ -1,3 +1,4 @@
+import { Optional } from '@/core/types/optional'
 import Entity from '../../../core/entities/entity'
 import Identity from '../../../core/entities/identity'
 import Email from '../../shared/email'
@@ -6,11 +7,24 @@ type EmployeeType = {
 	name: string
 	email: Email
 	password: string
+	createdAt: Date
+	updatedAt: Date
 }
 
 export default class Employee extends Entity<EmployeeType> {
-	static create(data: EmployeeType, id?: Identity) {
-		return new Employee(data, id)
+	static create(
+		data: Optional<EmployeeType, 'createdAt' | 'updatedAt'>,
+		id?: Identity
+	) {
+		const now = new Date()
+		return new Employee(
+			{
+				...data,
+				createdAt: data.createdAt ?? now,
+				updatedAt: data.updatedAt ?? now
+			},
+			id
+		)
 	}
 
 	get name() {
@@ -23,6 +37,14 @@ export default class Employee extends Entity<EmployeeType> {
 
 	get password() {
 		return this.attributes.password
+	}
+
+	get createdAt() {
+		return this.attributes.createdAt
+	}
+
+	get updatedAt() {
+		return this.attributes.updatedAt
 	}
 
 	set name(name: string) {
